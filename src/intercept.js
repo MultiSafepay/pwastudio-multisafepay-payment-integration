cd62ab769513c86d253311c816c173b9a4943e2a
Stanislau Kharlap <stanislau.kharlap@multisafepay.com>
2021-11-03 17:07:29 +0100

    added possibility for localization of plugin texts

--- src/intercept.js
+++ src/intercept.js
@@ -76,7 +76,7 @@ module.exports = targets => {
     ];
 
     targets.of('@magento/pwa-buildpack').specialFeatures.tap(flags => {
-        flags[targets.name] = {esModules: true, cssModules: true, graphqlQueries: true};
+        flags[targets.name] = {esModules: true, cssModules: true, graphqlQueries: true, i18n: true};
     });
 
     veniaTargets.routes.tap(
